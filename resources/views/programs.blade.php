9e10b50c2468cfac2bc05f86635c21873d7e10a4
Nurlan <nurlan.adiluly@gmail.com>
2019-12-08 03:42:08 +0600

    programs to blade

--- resources/views/programs.blade.php
+++ resources/views/programs.blade.php
@@ -17,91 +17,32 @@
                             <div class="col-lg-12 col-xl-12">
                                 <div class="card mb-4">
                                     <div class="card-body">
-
-                                            <div class="row">
-
+                                        <div class="row">
                                             <div class="owl-carousel">
-
-                                                <div class="col-md-12">
-                                                    <div class="ul-pricing__table-1">
-                                                        <div class="ul-pricing__image card-icon-bg-primary">
-                                                            <i class="i-Bicycle1"></i>
+                                                @foreach($programs as $program)
+                                                    <div class="col-md-12">
+                                                        <div class="ul-pricing__table-1">
+                                                            <div class="ul-pricing__image card-icon-bg-primary" style="background-image: url('{{ $program->image }}')">
+                                                                <i class="i-Bicycle1"></i>
+                                                            </div>
+                                                            <div class="ul-pricing__title">
+                                                                <h2 class="heading text-primary"> {{ $program->name }}</h2>
+                                                            </div>
+                                                            <div class="ul-pricing__text text-mute">{{ $program->duration }} дней</div>
+                                                            <div class="ul-pricing__main-number"> <h4 class="heading display-3 text-primary t-font-boldest" style="font-size: 3.5rem;">{{  number_format($program->price, 0,"."," ") }} ₸ </h4></div>
+                                                            <div class="ul-pricing__list">
+                                                               <p>{{ $program->description }} </p>
+                                                            </div>
+                                                            <button type="button" class="btn btn-lg btn-primary btn-rounded m-1">Выбрать и продолжить</button>
                                                         </div>
-                                                        <div class="ul-pricing__title">
-                                                            <h2 class="heading text-primary">Basic</h2>
-                                                        </div>
-                                                        <div class="ul-pricing__text text-mute">5gb</div>
-                                                        <div class="ul-pricing__main-number"> <h4 class="heading display-3 text-primary t-font-boldest" style="font-size: 3.5rem;">$10</h4></div>
-                                                        <div class="ul-pricing__list">
-                                                            {{ $val }}
-                                                           <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
-                                                        </div>
-                                                        <button type="button" class="btn btn-lg btn-primary btn-rounded m-1">Выбрать и продолжить</button>
                                                     </div>
-                                                </div>
-                                                <div class="col-md-12">
-                                                    <div class="ul-pricing__table-1">
-                                                        <div class="ul-pricing__image card-icon-bg-primary">
-                                                            <i class="i-Bicycle1"></i>
-                                                        </div>
-                                                        <div class="ul-pricing__title">
-                                                            <h2 class="heading text-primary">Basic</h2>
-                                                        </div>
-                                                        <div class="ul-pricing__text text-mute">5gb</div>
-                                                        <div class="ul-pricing__main-number"> <h4 class="heading display-3 text-primary t-font-boldest" style="font-size: 3.5rem;">$10</h4></div>
-                                                        <div class="ul-pricing__list">
-
-                                                           <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
-                                                        </div>
-                                                        <button type="button" class="btn btn-lg btn-primary btn-rounded m-1">Выбрать и продолжить</button>
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-12">
-                                                    <div class="ul-pricing__table-1">
-                                                        <div class="ul-pricing__image card-icon-bg-primary">
-                                                            <i class="i-Bicycle1"></i>
-                                                        </div>
-                                                        <div class="ul-pricing__title">
-                                                            <h2 class="heading text-primary">Basic</h2>
-                                                        </div>
-                                                        <div class="ul-pricing__text text-mute">5gb</div>
-                                                        <div class="ul-pricing__main-number"> <h4 class="heading display-3 text-primary t-font-boldest" style="font-size: 3.5rem;">$10</h4></div>
-                                                        <div class="ul-pricing__list">
-
-                                                           <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
-                                                        </div>
-                                                        <button type="button" class="btn btn-lg btn-primary btn-rounded m-1">Выбрать и продолжить</button>
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-12">
-                                                    <div class="ul-pricing__table-1">
-                                                        <div class="ul-pricing__image card-icon-bg-primary">
-                                                            <i class="i-Bicycle1"></i>
-                                                        </div>
-                                                        <div class="ul-pricing__title">
-                                                            <h2 class="heading text-primary">Basic</h2>
-                                                        </div>
-                                                        <div class="ul-pricing__text text-mute">5gb</div>
-                                                        <div class="ul-pricing__main-number"> <h4 class="heading display-3 text-primary t-font-boldest" style="font-size: 3.5rem;">$10</h4></div>
-                                                        <div class="ul-pricing__list">
-
-                                                           <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
-                                                        </div>
-                                                        <button type="button" class="btn btn-lg btn-primary btn-rounded m-1">Выбрать и продолжить</button>
-                                                    </div>
-                                                </div>
+                                                @endforeach
                                                 </div>
                                             </div>
                                     </div>
                                 </div>
-
-
-
-
-
                             </div>
                         </div>
-
                 </section>
 
 
