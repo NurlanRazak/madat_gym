43d94f79ca8c106b110f0608331621040f33c0d3
Nurdaulet <nurik9293709@gmail.com>
2019-12-14 10:18:37 +0600

    date fix

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -19,7 +19,8 @@ class ProfileController extends Controller
         $user = $request->user();
 
         $date_activation = Date::parse($user->subscriptions->first()->pivot->created_at)->format('d F Y');
-        $date_finish = Date::parse($user->subscriptions->first()->expires)->format('d F Y');
+        $last_subscr = $user->subscriptions()->latest()->first();
+        $date_finish = Date::parse(strtotime("{$user->subscriptions->first()->pivot->created_at} + {$last_subscr->days} days"))->format('d F Y');
         $dates = array($date_activation, $date_finish);
 
         $userparameters = Userparameter::where('user_id', $user->id)->get();
