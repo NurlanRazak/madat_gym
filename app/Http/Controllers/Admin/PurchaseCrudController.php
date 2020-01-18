e8fb1ce1192aa025ecaa42bf8ab79d7b7f8382cf
Nurlan <nurlan.adiluly@gmail.com>
2020-01-18 18:40:16 +0600

    hh to HH

--- app/Http/Controllers/Admin/PurchaseCrudController.php
+++ app/Http/Controllers/Admin/PurchaseCrudController.php
@@ -104,7 +104,7 @@ class PurchaseCrudController extends CrudController
                 'name' => 'start_date',
                 'label' => 'Дата покупки',
                 'type' => "datetime",
-                'format' => 'l h:m',
+                'format' => 'l HH:mm',
             ],
         ]);
 
