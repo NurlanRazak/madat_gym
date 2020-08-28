c0ccc3456a6d27ad9bf7430a83d4ea7fbee2e0df
Nurik <nurik9293709@gmail.com>
2020-08-29 03:35:22 +0600

    backpack admin routes fix

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -6,6 +6,15 @@
 // This route file is loaded automatically by Backpack\Base.
 // Routes you generate using Backpack\Generators will be placed here.
 
+Route::group(
+[
+    'namespace'  => 'App\Http\Controllers',
+    'middleware' => 'web',
+    'prefix'     => config('backpack.base.route_prefix'),
+], function () {
+    Route::post('edit-account-avatar', 'Auth\MyAccountController@postAvatarForm')->name('edit-account-avatar');
+});
+
 Route::group([
     'prefix'     => config('backpack.base.route_prefix', 'admin'),
     'middleware' => ['web', config('backpack.base.middleware_key', 'admin')],
